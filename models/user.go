4c192b70d4b3d5cf86dfc282d5cd183ed7de024b
sakuraea01 <sakuraea.78963@gmail.com>
2023-09-24 00:23:55 +0700

    DB_new + Login

--- models/user.go
+++ models/user.go
@@ -2,7 +2,7 @@ package models
 
 type User struct {
 	ID      uint   `json:"id" gorm:"primaryKey"`
-	User_ID uint   `json:"user_id" gorm:"unique;not null;default: null"`
+	User_ID uint   `json:"user_id" gorm:"unique;default: null"`
 	Email   string `json:"email" gorm:"unique;not null;default: null"`
 	Fname   string `json:"first_name" gorm:"not null;default: null"`
 	Lname   string `json:"last_name" gorm:"not null;default: null"`
