9174e61ffd8ab1fa7472c4f179d9df496692923d
sakuraea01 <sakuraea.78963@gmail.com>
2023-11-15 02:20:24 +0700

    AAAA

--- db/db.go
+++ db/db.go
@@ -2,16 +2,16 @@ package db
 
 import (
 	"fmt"
+	"go-fiber-api-docker/config"
 	"go-fiber-api-docker/models"
 	"log"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"github.com/gofiber/storage/postgres/v3"
 )
 
 func Init(c *config.Config) *gorm.DB {
-	url := fmt.Sprintf("postgres://postgres:G2gFF36dced5b-EcAG-1DGGF-aGe2aAF@roundhouse.proxy.rlwy.net:26379/railway")
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", c.DBUser, c.DBPass, c.DBHost, c.DBPort, c.DBName)
 	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
 
 	if err != nil {
@@ -19,8 +19,11 @@ func Init(c *config.Config) *gorm.DB {
 	}
 
 	db.AutoMigrate(&models.User{})
+	db.AutoMigrate(&models.Request{})
+	db.AutoMigrate(&models.RequestReservation{})
 	db.AutoMigrate(&models.ReservationTime{})
 	db.AutoMigrate(&models.Room{})
+	db.AutoMigrate(&models.Course{})
 
 	return db
 }
